fd4672bdace43c4048fa4ff1094a468577839748
Nora Granahan <namgranahan@gmail.com>
2018-04-29 17:46:39 -0400

    wrote code for server and routing using temp html

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var PORT = process.env.PORT || 3000;
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
